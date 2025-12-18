eed7af801ebd5801117b70b67cdd7d5f21dd2c37
bala-amypo <bala@amypo.in>
2025-12-18 16:46:50 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/AppUserRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+import com.example.demo.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+public interface AppUserRepository extends JpaRepository<AppUser,Long>{
+    List<AppUser>findByEmail(Long patientId);
+}
--- src/main/java/com/example/demo/repository/DeviationRuleRepository.java
+++ src/main/java/com/example/demo/repository/DeviationRuleRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repository;
-import com.example.demo.entity.RecoveryCurveProfile;
+import com.example.demo.entity.DeviationRule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-public interface RecoveryCurveRepository extends JpaRepository<RecoveryCurveProfile,Long>{
-    List<RecoveryCurveProfile>findBySurgeryType(String surgeryType);
+public interface DeviationRuleRepository extends JpaRepository<DeviationRule,Long>{
+    List<DeviationRule>findBySurgeryType(String surgeryType);
 }
