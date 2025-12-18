213e3610dc178affd638fceb8b22ffa01889202b
bala-amypo <bala@amypo.in>
2025-12-18 08:44:42 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/PatientProfile.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PatientProfile {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+            private Long id;
+        @Column(unique = true)
+            private String patientId;
+            private String fullName;
+            private Integer age;
+        @Column(unique = true)
+            private String email;
+            private String surgeryType;
+            private Boolean active;
+                                                    private LocalDateTime createdAt;
+}
