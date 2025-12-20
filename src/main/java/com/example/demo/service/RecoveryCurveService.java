d707717f8d8d8e9c997b2af0f93ee0159e8f3c0f
bala-amypo <bala@amypo.in>
2025-12-20 07:57:19 +0000

    User commit

--- src/main/java/com/example/demo/service/RecoveryCurveService.java
+++ src/main/java/com/example/demo/service/RecoveryCurveService.java
@@ -4,41 +4,41 @@ import com.example.demo.entity.RecoveryCurveProfile;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.RecoveryCurveProfileRepository;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
-
 public class RecoveryCurveService {
+
     private final RecoveryCurveProfileRepository recoveryCurveProfileRepository;
+
     public RecoveryCurveService(RecoveryCurveProfileRepository recoveryCurveProfileRepository) {
         this.recoveryCurveProfileRepository = recoveryCurveProfileRepository;
-
     }
+
+    // Create Recovery Curve
     public RecoveryCurveProfile createRecoveryCurve(RecoveryCurveProfile profile) {
 
         if (profile.getExpectedPainLevel() < 0
-        
-        || profile.getExpectedMobilityLevel() < 0
-
-        || profile.getExpectedFatigueLevel() < 0) {
+                || profile.getExpectedMobilityLevel() < 0
+                || profile.getExpectedFatigueLevel() < 0) {
 
             throw new IllegalArgumentException("Threshold must be non-negative");
-
         }
 
         return recoveryCurveProfileRepository.save(profile);
-
     }
+
+    // Get Recovery Curves by Surgery Type
     public List<RecoveryCurveProfile> getRecoveryCurvesBySurgeryType(String surgeryType) {
 
         List<RecoveryCurveProfile> curves =
+                recoveryCurveProfileRepository.findBySurgeryType(surgeryType);
 
-        recoveryCurveProfileRepository.findBySurgeryType(surgeryType);
         if (curves.isEmpty()) {
             throw new ResourceNotFoundException("Recovery curve not found");
         }
+
         return curves;
-
     }
-
 }
