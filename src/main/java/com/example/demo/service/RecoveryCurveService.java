e012bd01e3b547b27fcdd1540ff8c3799e2a1d34
bala-amypo <bala@amypo.in>
2025-12-20 08:06:19 +0000

    User commit

--- src/main/java/com/example/demo/service/RecoveryCurveService.java
+++ src/main/java/com/example/demo/service/RecoveryCurveService.java
@@ -9,16 +9,12 @@ import java.util.List;
 
 @Service
 public class RecoveryCurveService {
-
     private final RecoveryCurveRepository recoveryCurveRepository;
-
     public RecoveryCurveService(RecoveryCurveRepository recoveryCurveRepository) {
         this.recoveryCurveRepository = recoveryCurveRepository;
     }
 
-    // Create Recovery Curve
     public RecoveryCurveProfile createRecoveryCurve(RecoveryCurveProfile profile) {
-
         if (profile.getExpectedPainLevel() < 0
                 || profile.getExpectedMobilityLevel() < 0
                 || profile.getExpectedFatigueLevel() < 0) {
@@ -28,17 +24,12 @@ public class RecoveryCurveService {
 
         return recoveryCurveRepository.save(profile);
     }
-
-    // Get Recovery Curves by Surgery Type
     public List<RecoveryCurveProfile> getRecoveryCurvesBySurgeryType(String surgeryType) {
-
         List<RecoveryCurveProfile> curves =
                 recoveryCurveRepository.findBySurgeryType(surgeryType);
-
         if (curves.isEmpty()) {
             throw new ResourceNotFoundException("Recovery curve not found");
         }
-
         return curves;
     }
 }
