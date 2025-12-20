5ea0314b3bdddf2cb73506e9657f03c23cea61f6
bala-amypo <bala@amypo.in>
2025-12-20 07:51:20 +0000

    User commit

--- src/main/java/com/example/demo/service/ClinicalAlertService.java
+++ src/main/java/com/example/demo/service/ClinicalAlertService.java
@@ -17,8 +17,8 @@ public class ClinicalAlertService {
 
 
     public ClinicalAlertService(ClinicalAlertRepository clinicalAlertRepository,
-    )
-    PatientProfileRepository patientProfileRepository{
+    
+    PatientProfileRepository patientProfileRepository){
 
         this.clinicalAlertRepository = clinicalAlertRepository;
 
