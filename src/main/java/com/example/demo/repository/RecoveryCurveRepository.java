e56be03a9551ce07389c93cf333cdeed0b2da935
bala-amypo <bala@amypo.in>
2025-12-21 06:30:44 +0000

    User commit

--- src/main/java/com/example/demo/repository/RecoveryCurveRepository.java
+++ src/main/java/com/example/demo/repository/RecoveryCurveRepository.java
@@ -3,5 +3,5 @@ import com.example.demo.entity.RecoveryCurveProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 public interface RecoveryCurveRepository extends JpaRepository<RecoveryCurveProfile,Long>{
-    List<RecoveryCurveProfile>findBySurgeryType(String surgeryType);
+    List<RecoveryCurveProfile> findByExpectedPainLevel(Integer level);
 }
