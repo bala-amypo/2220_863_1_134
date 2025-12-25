6d75f14fa91c4a5fe7ba39e3115fb678fbd886ab
bala-amypo <bala@amypo.in>
2025-12-25 17:26:26 +0000

    User commit

--- src/main/java/com/example/demo/repository/DeviationRuleRepository.java
+++ src/main/java/com/example/demo/repository/DeviationRuleRepository.java
@@ -1,5 +1,5 @@
 package com.example.demo.repository;
-import com.example.demo.entity.DeviationRule;
+import com.example.demo.model.DeviationRule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 public interface DeviationRuleRepository extends JpaRepository<DeviationRule,Long>{
